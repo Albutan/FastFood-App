902362106e2c6b3052d08d3a86ba9d7a43a0b0d7
Juan Camilo <juansoad.123@gmail.com>
2022-10-28 15:50:44 -0500

    ajuste css de ventanas

--- FastFood-App/src/app/services/order.service.ts
+++ FastFood-App/src/app/services/order.service.ts
@@ -1,14 +1,17 @@
 import { Order } from './../models/order';
 import { Injectable } from '@angular/core';
+import * as _ from 'lodash';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrderService {
   private _order: Order;
+  private _numOrder: number;
 
   constructor() {
     this._order = new Order({});
+    this._numOrder = 1;
   }
 
   get order(): Order {
@@ -17,4 +20,26 @@ export class OrderService {
   set order(value: Order) {
     this._order = value;
   }
+
+  convertOrder() {
+    const finalOrder: any = {
+      products: [],
+      date: new Date(),
+      numOrder: this._numOrder,
+      priceOrder: this.order.totalOrder,
+    }
+    _.forEach(this.order.productsOrder, product => {
+      const finalProduct = {
+        "name": product.name,
+        "priceFinal": product.totalOrder() * product.quantity,
+        "extras": product.getExtras(),
+        "quantity": product.quantity
+      }
+      finalOrder.products.push(finalProduct);
+    })
+    this._numOrder++;
+    return finalOrder;
+  }
+
+
 }
